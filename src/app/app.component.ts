a4d797f4b00137025cc65b3c802b9baf37a1ffe5
Niels Koster <n.koster@portavita.eu>
2018-01-24 23:01:17 +0100

    Ionic authentication base with okta

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,19 +4,28 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { TabsPage } from '../pages/tabs/tabs';
+import { LoginPage } from '../pages/login/login';
+
+import { OAuthService } from 'angular-oauth2-oidc';
 
 @Component({
-  templateUrl: 'app.html'
+    templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = TabsPage;
+    rootPage: any = TabsPage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
-    platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
-      statusBar.styleDefault();
-      splashScreen.hide();
-    });
-  }
+    constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen,
+                oauthService: OAuthService) {
+        if (oauthService.hasValidIdToken()) {
+            this.rootPage = TabsPage;
+        } else {
+            console.log('*** no valid token!');
+            this.rootPage = LoginPage;
+        }
+
+        platform.ready().then(() => {
+            statusBar.styleDefault();
+            splashScreen.hide();
+        });
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,34 +7,45 @@ import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
+import { LoginPage } from '../pages/login/login';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { OAuthService } from "angular-oauth2-oidc";
+import { HttpModule} from "@angular/http";
+import { HttpClientModule } from '@angular/common/http';
+import { UrlHelperService} from "angular-oauth2-oidc";
 
 @NgModule({
-  declarations: [
-    MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
-    TabsPage
-  ],
-  imports: [
-    BrowserModule,
-    IonicModule.forRoot(MyApp)
-  ],
-  bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
-    TabsPage
-  ],
-  providers: [
-    StatusBar,
-    SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
-  ]
+    declarations: [
+        MyApp,
+        AboutPage,
+        ContactPage,
+        HomePage,
+        TabsPage,
+        LoginPage
+    ],
+    imports: [
+        BrowserModule,
+        HttpModule,
+        HttpClientModule,
+        IonicModule.forRoot(MyApp)
+    ],
+    bootstrap: [IonicApp],
+    entryComponents: [
+        MyApp,
+        AboutPage,
+        ContactPage,
+        HomePage,
+        TabsPage,
+        LoginPage
+    ],
+    providers: [
+        OAuthService,
+        UrlHelperService,
+        StatusBar,
+        SplashScreen,
+        {provide: ErrorHandler, useClass: IonicErrorHandler}
+    ]
 })
 export class AppModule {}
